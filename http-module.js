f74fbaad33e14170409f3e2add66d4e45cad4350
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2021-04-30 13:34:22 +0600

    Compltted app.js

--- http-module.js
+++ http-module.js
@@ -1,9 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    if(req.url =="/"){
+        res.write("<h1> This is Base URL</h1>");
+        res.end();
+    } else if(req.url == "/home") {
+        res.write("<h1> This is the home page</h1>");
+        res.end();
+    } else {
+        res.write("<h1> This page doesn't exist</h1>");
+        res.end();
+    }
+    /*res.writeHead(201, {"Content-Type" : "text/plain"});
     res.write("<h1>Hello</h1>");
-    res.end();
+    res.end();*/
 });
 
-server.listen(7777);
+module.exports = {server};
